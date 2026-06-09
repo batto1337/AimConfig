58e7b518e32ec2ed760effca5ec5c30842df85f1
batto1337 <bw9399@gmail.com>
2026-06-09 15:02:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function showMessage() {
   alert("Witaj w AimConfig!");
 }
+
+let visits = localStorage.getItem("visits");
+
+if (visits === null) {
+  visits = 1;
+} else {
+  visits = Number(visits) + 1;
+}
+
+localStorage.setItem("visits", visits);
+
+const counter = document.getElementById("visitor-counter");
+
+if (counter) {
+  counter.textContent = "Odwiedziny: " + visits;
+}
